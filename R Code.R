760573fe7293a10140778c97f7d1fe2a2ebe24a2
Samuel Knopka <sck0011@mix.wvu.edu>
2018-10-17 09:41:23 -0400

    Initial Commit

---
+++ R Code.R
@@ -0,0 +1,28 @@
+# doing addition
+5+3
+# assigning variables a value
+x <- 3
+5 + x
+
+variable <- 56
+
+variable + x
+y <- 'a letter'
+
+class(x)
+class(y)
+
+z <- matrix(data = 1:16, ncol = 4, nrow = 4, byrow = T)
+class(z)
+
+a <- 1 == 2
+
+plot()
+# load package
+library(ggplot2)
+ggplot()
+
+# functions
+"add function" <- function(a,b)
+  
+  
